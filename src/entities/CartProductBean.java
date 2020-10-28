1e206fa698bd5efc59c75f571479b5d5f7447e4d
Pablo Gómez-Caldito <pgomezcaldito@idealista.com>
2020-10-28 08:05:29 +0100

    Add CartBean.java and CartProductBean.java and include them in the persistence.xml

---
+++ src/entities/CartProductBean.java
@@ -0,0 +1,82 @@
+package entities;
+
+import static javax.persistence.GenerationType.AUTO;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name="cartproducts")
+public class CartProductBean {
+	
+	// Attributes
+	@Id
+	@GeneratedValue (strategy = AUTO)
+	private int id;
+	
+	@Column(name="cart")
+	@NotNull
+	private int cart;
+	
+	@Column(name="product")
+	@NotNull
+	private int product;
+	
+	@Column(name="quantity")
+	@NotNull
+	private int quantity;
+
+	
+	// Constructors
+	public CartProductBean() {
+
+	}
+	
+	public CartProductBean(int idCart, int idItem, int quantity) {
+		this.cart = idCart;
+		this.product = idItem;
+		this.quantity = quantity;
+	}
+
+	
+	// Getters and setters
+	public int getId() {
+		return id;
+	}
+
+	public int getCart() {
+		return cart;
+	}
+
+	public void setCart(int cart) {
+		this.cart = cart;
+	}
+
+	public int getProduct() {
+		return product;
+	}
+
+	public void setProduct(int product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+
+
+	
+	
+
+}
