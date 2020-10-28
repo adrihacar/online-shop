9284769e2ffef10b01c367920cbcc188bad1cbf7
Pablo Gómez-Caldito <pgomezcaldito@idealista.com>
2020-10-28 19:53:00 +0100

    Inserts in dbsetup.sql

--- dbsetup.sql
+++ dbsetup.sql
@@ -19,7 +19,7 @@ CREATE TABLE PRODUCTS (
     status INTEGER NOT NULL,
     category INTEGER NOT NULL,
     description TEXT NOT NULL,
-    image BLOB NOT NULL,
+    image BLOB,
     price DOUBLE NOT NULL,
     
     FOREIGN KEY(seller) REFERENCES USERS(id)
@@ -31,7 +31,7 @@ CREATE TABLE CARTS (
     address VARCHAR(256) NOT NULL,
     paymethod INTEGER NOT NULL,
     bought BOOLEAN NOT NULL,
-    date VARCHAR(16) NOT NULL,
+    date BIGINT NOT NULL,
 
     FOREIGN KEY(user) REFERENCES USERS(id)
 );
@@ -45,3 +45,23 @@ CREATE TABLE CARTPRODUCTS (
     FOREIGN KEY(cart) REFERENCES CARTS(id),
     FOREIGN KEY(product) REFERENCES PRODUCTS(id)
 );
+
+INSERT INTO USERS (name, surname, email, location, password) VALUES
+  ("John","Doe","email1@gmail.com","madrid","asdfaszd"),
+  ("Jane","Doe","email2@gmail.com","madrid","asdfaszd"),
+  ("Fulanito","Doe","email3@gmail.com","madrid","asdfaszd"),
+  ("Fulanita","Doe","email4gmail.com","madrid","asdfaszd");
+  
+INSERT INTO PRODUCTS (seller, name, status, category, description, price) VALUES
+  (1,"Paper",0,8,"asdfaszd",4.9),
+  (1,"keyboard",0,6,"asdfaszd",2.7),
+  (3,"Pencil",0,9,"asdfaszd",0.7),
+  (2,"Gums",0,1,"asdfaszd",2.2);
+  
+INSERT INTO CARTS (user, address, paymethod, bought, date) VALUES
+  (2,"Mordor",1,true,12312381);
+  
+INSERT INTO CARTPRODUCTS (cart, product, quantity) VALUES
+  (1,3,2),
+  (1,1,1);
+
