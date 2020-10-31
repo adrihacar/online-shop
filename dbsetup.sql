141be305635f8604fe550a690c9f11f13a2db2f6
Pablo Gómez-Caldito <pgomezcaldito@idealista.com>
2020-10-31 10:18:05 +0100

    Add salt and rename tables to lowercase

--- dbsetup.sql
+++ dbsetup.sql
@@ -1,18 +1,19 @@
-DROP TABLE CARTPRODUCTS;
-DROP TABLE CARTS;
-DROP TABLE PRODUCTS;
-DROP TABLE USERS;
+DROP TABLE cartproducts;
+DROP TABLE carts;
+DROP TABLE products;
+DROP TABLE users;
 
-CREATE TABLE USERS (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     surname VARCHAR(256) NOT NULL,
     email VARCHAR(256) UNIQUE NOT NULL,
     location VARCHAR(256) NOT NULL,
-    password VARCHAR(256) NOT NULL
+    password VARCHAR(256) NOT NULL,
+    salt TEXT NOT NULL
 );
 
-CREATE TABLE PRODUCTS (
+CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     seller BIGINT UNSIGNED NOT NULL,
     name VARCHAR(256) NOT NULL,
@@ -22,10 +23,10 @@ CREATE TABLE PRODUCTS (
     image BLOB,
     price DOUBLE NOT NULL,
     
-    FOREIGN KEY(seller) REFERENCES USERS(id)
+    FOREIGN KEY(seller) REFERENCES users(id)
 );
 
-CREATE TABLE CARTS (
+CREATE TABLE carts (
     id SERIAL PRIMARY KEY,
     user BIGINT UNSIGNED NOT NULL,
     address VARCHAR(256) NOT NULL,
@@ -33,35 +34,35 @@ CREATE TABLE CARTS (
     bought BOOLEAN NOT NULL,
     date BIGINT NOT NULL,
 
-    FOREIGN KEY(user) REFERENCES USERS(id)
+    FOREIGN KEY(user) REFERENCES users(id)
 );
 
-CREATE TABLE CARTPRODUCTS (
+CREATE TABLE cartproducts (
     id SERIAL PRIMARY KEY,
     cart BIGINT UNSIGNED NOT NULL,
     product BIGINT UNSIGNED NOT NULL,
     quantity INTEGER NOT NULL,
 
-    FOREIGN KEY(cart) REFERENCES CARTS(id),
-    FOREIGN KEY(product) REFERENCES PRODUCTS(id)
+    FOREIGN KEY(cart) REFERENCES carts(id),
+    FOREIGN KEY(product) REFERENCES products(id)
 );
 
-INSERT INTO USERS (name, surname, email, location, password) VALUES
-  ("John","Doe","email1@gmail.com","madrid","asdfaszd"),
-  ("Jane","Doe","email2@gmail.com","madrid","asdfaszd"),
-  ("Fulanito","Doe","email3@gmail.com","madrid","asdfaszd"),
-  ("Fulanita","Doe","email4gmail.com","madrid","asdfaszd");
+INSERT INTO users (name, surname, email, location, password, salt) VALUES
+  ("John","Doe","email1@gmail.com","madrid","asdfaszd","asdf"),
+  ("Jane","Doe","email2@gmail.com","madrid","asdfaszd","asdf"),
+  ("Fulanito","Doe","email3@gmail.com","madrid","asdfaszd","asdf"),
+  ("Fulanita","Doe","email4gmail.com","madrid","asdfaszd","asdf");
   
-INSERT INTO PRODUCTS (seller, name, status, category, description, price) VALUES
+INSERT INTO products (seller, name, status, category, description, price) VALUES
   (1,"Paper",0,8,"asdfaszd",4.9),
   (1,"keyboard",0,6,"asdfaszd",2.7),
   (3,"Pencil",0,9,"asdfaszd",0.7),
   (2,"Gums",0,1,"asdfaszd",2.2);
   
-INSERT INTO CARTS (user, address, paymethod, bought, date) VALUES
+INSERT INTO carts (user, address, paymethod, bought, date) VALUES
   (2,"Mordor",1,true,12312381);
   
-INSERT INTO CARTPRODUCTS (cart, product, quantity) VALUES
+INSERT INTO cartproducts (cart, product, quantity) VALUES
   (1,3,2),
   (1,1,1);
 
